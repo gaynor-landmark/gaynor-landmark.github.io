8c1f04f45053dd10ec1334d036c65d544616671b
gaynor-landmark <g.l.landmark@gmail.com>
2016-01-13 15:32:14 +1300

    add drop down nav menus and random pics

---
+++ scripts/indexscript.js
@@ -0,0 +1,42 @@
+
+function picNum(){
+  // return a random number between 1 and 10
+  return (Math.floor(Math.random() * 10) + 1);
+}
+
+function dropReflectionsNav(){
+  console.log ("ref");
+  document.getElementById("reflectionsLinks").classList.toggle("show");
+  //close projects if they are open
+  var dropdownContent = document.getElementById("projectsLinks");
+  if (dropdownContent.classList.contains('show')){
+    dropdownContent.classList.remove('show');
+  } 
+}
+
+function dropProjectsNav(){
+  console.log("pro") ;
+  document.getElementById("projectsLinks").classList.toggle("show");
+  //close reflections if they are open
+  var dropdownContent = document.getElementById("reflectionsLinks");
+  if (dropdownContent.classList.contains('show')){
+    dropdownContent.classList.remove('show');
+  }
+}
+
+window.onclick = function(e){
+  if (!e.target.matches("#reflections")){
+   
+
+
+    
+  }
+}
+
+$(document).ready(function(){
+  // load the bottom three pictures -select randomly from the set of ten
+  for (var i = 1; i < 4; i++){
+    var riverPic = document.getElementById("riverPic" + i);   
+    riverPic.src = "./images/Bend-in-the-river" + picNum() + ".jpg";
+  };
+});
