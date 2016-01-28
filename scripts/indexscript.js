e945007a71f1003f832367ddab06fdb3ce426c46
gaynor-landmark <g.l.landmark@gmail.com>
2016-01-29 09:42:01 +1300

    add content to compassion page

--- scripts/indexscript.js
+++ scripts/indexscript.js
@@ -1,7 +1,7 @@
 
 function picNum(){
-  // return a random number between 1 and 10
-  return (Math.floor(Math.random() * 11) + 1);
+  // return a random number between 1 and 12
+  return (Math.floor(Math.random() * 12) + 1);
 }
 
 function dropReflectionsNav(){
