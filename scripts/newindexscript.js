c15eeedd53e879417fe69f0656c14ac64d7fef85
gaynor-landmark <g.l.landmark@gmail.com>
2016-09-17 20:19:40 +1200

    project links

--- scripts/newindexscript.js
+++ scripts/newindexscript.js
@@ -13,3 +13,20 @@ function picNum(){
   // return a random number between 1 and 11
   return (Math.floor(Math.random() * 11) + 1);
 }
+
+document.onclick = function(e){
+
+  if (e.target.id !== 'reflectionsID') {
+    //close reflections if they are open
+    var refdropdownContent = document.getElementById("reflectionsLinks");
+    if (refdropdownContent.classList.contains('show')){
+      refdropdownContent.classList.remove('show');
+    }
+  }
+
+}
+function dropReflectionsNav(){
+  console.log ("ref");
+  document.getElementById("reflectionsLinks").classList.toggle("show");
+  
+}
